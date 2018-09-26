9fe3749e4fd14135923909fbb2b1e80ef5fcc297
sspan <sspantiezheng@gmail.com>
2018-09-26 20:00:42 +0800

    create production settings

--- falab/settings.py
+++ falab/settings.py
@@ -127,3 +127,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Production
+# try:
+#     from production import *
+# except:
+#     pass
