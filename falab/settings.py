e5b60f4298a577d74958b4b402483b593f9ee6e7
sspantz <sspantiezheng@gmail.com>
2018-09-27 21:34:45 +0800

    add logo to header.html

--- falab/settings.py
+++ falab/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "falab/static"),
+    os.path.join(BASE_DIR, "falab/static/"),
 ]
 
 STATIC_URL = '/static/'
