dfd13f661ae096cfb815bdf0c84c7dcda45167f3
sspantz <sspantiezheng@gmail.com>
2018-09-28 13:41:36 +0800

    update production option for settings

--- falab/settings.py
+++ falab/settings.py
@@ -130,7 +130,7 @@ MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Production
-# try:
-#     from .production import *
-# except:
-#     pass
+try:
+    from .production import *
+except:
+    pass
